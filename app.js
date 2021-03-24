dcd801fcdbb2be62943b1bec0299226f353dccf0
RakibRahman <md.rr.talukder@gmail.com>
2021-03-24 10:15:05 +0600

    basic coding and styles done

---
+++ app.js
@@ -0,0 +1,29 @@
+const copy = document.getElementById("copy-btn");
+const textarea = document.getElementById("textarea");
+const speak = document.getElementById("speak-btn");
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+speak.addEventListener("click", function () {
+  recognition.start(); //?Starts the speech recognition service listening to incoming audio
+  textarea.innerHTML = "...listening to you 👂";
+});
+
+recognition.onresult = function (e) {
+  var transcript = e.results[0][0].transcript; //checkout mdn for details
+  textarea.innerHTML = transcript;
+  textarea.style.fontSize = "2rem";
+  textarea.style.color = "#f14e0d";
+};
+
+//!clipboard
+
+copy.addEventListener("click", function () {
+  textarea.select();
+  textarea.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  Swal.fire("Copied the text");
+});
